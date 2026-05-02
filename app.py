633f926461669e7d8365f328222a1333844afd4c
Keith-Git777 <keith.basile@mail.wlc.edu>
2026-05-02 16:51:12 -0500

    F app.py

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-import streamlit as st
-st.title('My First Official Streamlit App')
-st.write('Hello! This app is live on Streamlit Community Cloud.')
-name = st.text_input('Enter your name:')
-if name:
-st.write(f'Welcome, {name}!')
+
